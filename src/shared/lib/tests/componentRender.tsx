ef78482f0af17783b0ce139693976c9db58d95c9
Gearonix <egor.uzhanin@gmail.com>
2023-02-19 18:49:26 +0300

    added json server and started testing thunks, action, redux

--- src/shared/lib/tests/componentRender.tsx
+++ src/shared/lib/tests/componentRender.tsx
@@ -4,7 +4,7 @@ import i18nMock from 'shared/config/i18n/i18nForTests'
 import {render} from '@testing-library/react'
 import {Provider} from 'react-redux'
 import {configureStore, DeepPartial} from '@reduxjs/toolkit'
-import {StateSchema} from './../../../app/providers/StoreProvider'
+import {StateSchema} from 'app/providers/StoreProvider'
 import {counterReducer} from 'entities/Counter'
 
 
